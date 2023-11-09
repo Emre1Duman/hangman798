5b37dead731bc49be19c2ae42aac024ff1093542
Emre Duman <emre@Emres-MacBook-Air-5.local>
2023-11-09 19:34:38 +0000

    reorganised

--- milestone_2.py
+++ milestone_2.py
@@ -1,13 +1,8 @@
 import random
 word_list = ["Strawberry", "Apple", "Cherry", "Banana", "Watermelon"]
-
-print(word_list)
-
 word = random.choice(word_list)
-print(word)
 
 guess = input("Guess a letter: ")
-
 
 
 if len(guess) == 1 and guess.isdigit() == False:
